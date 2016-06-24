b0df45bd7429bbfcffb9519c3418c8980e19da51
Sherif Abdelhamid <sherief@vt.edu>
2016-06-24 13:39:29 -0400

    created admin user, added new model "Reviewer"

--- peerreviews/models.py
+++ peerreviews/models.py
@@ -1,5 +1,22 @@
-from __future__ import unicode_literals
-
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Reviewer(models.Model):
+
+    name = models.CharField(max_length=200)
+    affiliation = models.TextField()
+    email = models.EmailField
+    bio = models.TextField
+    research = models.TextField
+    website = models.URLField
+    member_date = models.DateTimeField(
+            default=timezone.now)
+    number_reviews = models.IntegerField
+
+    #def countReviews(self):
+    # add here code to count number of reviews
+    #self.number_reviews = xxx
+    #self.save()
+
+
